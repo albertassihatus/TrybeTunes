91f6a6b455d70317b488ce6a31f2b63902fc2d48
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-07-01 15:51:58 -0300

    requisito 1 ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import Search from './Pages/Search';
+import Album from './Pages/Album';
+import Favorites from './Pages/Favorites';
+import Profile from './Pages/Profile';
+import Notfnd from './Pages/Notfnd';
+import ProfileEdit from './Pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <main>
+        <Route path="/" component={ Login } exact />
+        <Route path="/search" component={ Search } />
+        <Route path="/album" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } exact />
+        <Route path="*" component={ Notfnd } />
+      </main>
+    );
   }
 }
 
---
+++ src/Pages/Favorites.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Favorites extends React.Component {
+  render() {
+    return (
+      <h1 data-testid="page-favorites">Olá sou o Favorites</h1>
+    );
+  }
+}
+
+export default Favorites;
