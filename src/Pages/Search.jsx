c42f39c4c37da479e48f5366d4d9d34e8ed310bd
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-07-05 13:44:45 -0300

    req5

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -2,11 +2,80 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      // loading: false,
+      // next: false,
+    };
+  }
+
+  onSaveButton = async () => {
+    // const { name } = this.state;
+    // this.setState({ loading: true });
+    // await createUser({ name }).then((e) => {
+    //   if (e) {
+    //     this.setState({ next: true });
+    //   }
+    // });
+  }
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      name: value,
+    });
+  }
+
+  validateForm = () => {
+    const { name } = this.state;
+
+    const minLength = 2;
+    const anyInfo = name.length >= minLength;
+
+    if (anyInfo) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
+    const { name } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <main>
+
+          {/* {loading && <Loading />}
+          {next && <Redirect to="/search" />} */}
+
+          <form>
+            <label htmlFor="name">
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                name="name"
+                id="name"
+                value={ name }
+                onChange={ this.onInputChange }
+                placeholder="Artist"
+              />
+            </label>
+            <br />
+            <p />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ this.validateForm() }
+              onChange={ this.onInputChange }
+              onClick={ this.onSaveButton }
+            >
+              Pesquisar
+
+            </button>
+          </form>
+        </main>
       </div>
     );
   }
